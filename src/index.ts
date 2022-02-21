d6039647d8718d4c098a23aef54b25540a2e0920
mobeenaftab <mobeenaftab1@gmail.com>
2022-02-21 21:26:44 +0000

    Add project dependencies and setup file structure

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
